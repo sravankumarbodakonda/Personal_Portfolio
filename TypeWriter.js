e7a763c31d6ba5303510824dcaae6f2b37b6d6b6
sravan123-project <bodakondasravankumar@gmail.com>
2025-02-07 22:39:18 -0500

    changes made

--- TypeWriter.js
+++ TypeWriter.js
@@ -2,7 +2,7 @@
 const typedTextSpan = document.querySelector(".typed-text");
 const cursorSpan = document.querySelector(".cursor");
 
-const textArray = ["Sravan Kumar","Web Developer"];
+const textArray = ["Sravan Kumar","Web Developer","Salesforce Developer"];
 const typingDelay = 150;
 const erasingDelay = 50;
 const newTextDelay = 1500; // Delay between current and next text
